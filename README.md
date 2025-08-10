e3c8bb0283dc5d7fdacc7bac19bb70794bbd1108
jnores <josenores89@gmail.com>
2025-08-10 00:03:14 -0300

    feat(acumualdores): Se define la clase de pruenas y el esqueleto de la clase a implementar por los estudiantes.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # tarea00-acumuladores
-Tarea de acumuladores booleanos
+
+En el moodle encontrarán el enunciado completo con ejemplos de uso de las funciones. 
+
+Para configurar jUnit en eclipse y poder ejecutar los tests pueden guiarse con el video en el canal de la materia: 
+[Test unitario - Instalacion y uso de JUnit 4](https://www.youtube.com/watch?v=spnT2NjAxD0)
+
+> [! IMPORTANTE]
+> Pasar los tests no garantiza que la implementación sea correcta, pero lo hacemos porque
+> **“NO PASAR TODOS LOS TEST SIGNIFICA QUE LA IMPLEMENTACIÓN NO ES CORRECTA”**.
---
+++ src/acumuladores/Acumuladores.java
@@ -0,0 +1,65 @@
+package acumuladores;
+
+public class Acumuladores {
+
+	/**
+	 * Dada una matriz de enteros y un número, verifica si existe alguna fila 
+	 * donde todos sus elementos sean múltiplos del número recibido por 
+	 * parámetro.
+	 * 
+	 * Si la matriz está vacía o si el número no es positivo, devuelve falso.
+	 * 
+	 * @param mat
+	 * @param num
+	 * @return
+	 */
+	public boolean todosMultiplosEnAlgunaFila(int[][] mat, int num) { 
+		throw new RuntimeException("Metodo no implementado aun!!!");
+	}
+	
+	/**
+	 * Dado 2 matrices se verifica si hay intersección entre las filas de cada
+	 * matriz, fila a fila.
+	 * 
+	 * Si las matrices tienen distinta cantidad de filas o si alguna matriz 
+	 * está vacía, devuelve falso.
+	 * 
+	 * @param mat1
+	 * @param mat2
+	 * @return
+	 */
+	public boolean hayInterseccionPorFila(int[][] mat1, int[][]mat2) { 
+		throw new RuntimeException("Metodo no implementado aun!!!");
+	}
+	
+	/**
+	 * Dada una matriz y el índice de una columna, se verifica si existe alguna
+	 * fila cuya suma de todos sus elementos sea mayor estricto que la suma de
+	 * todos los elementos de la columna indicada por parámetro.
+	 * 
+	 * Si el índice de la columna es inválido o la matriz está vacía, devuelve 
+	 * falso.
+	 * 
+	 * @param mat
+	 * @param nColum
+	 * @return
+	 */
+	public boolean algunaFilaSumaMasQueLaColumna(int[][] mat, int nColum) { 
+		throw new RuntimeException("Metodo no implementado aun!!!");
+	}
+	
+	/**
+	 * Dadas 2 matrices, se verifica si hay intersección entre las columnas de
+	 * cada matriz, columna a columna.
+	 * 
+	 * Si las matrices tienen distinta cantidad de columnas o alguna matriz 
+	 * está vacía, devuelve falso. 
+	 * 
+	 * @param mat1
+	 * @param mat2
+	 * @return
+	 */
+	public boolean hayInterseccionPorColumna(int[][] mat1, int[][]mat2) { 
+		throw new RuntimeException("Metodo no implementado aun!!!");
+	}
+}
